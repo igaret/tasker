8aa0b03a5952bd29d8fb6fdd0cc530116722ae5f
igaret <148014802+igaret@users.noreply.github.com>
2025-03-01 00:49:01 -0800

    Update rish.sh

--- Init.d/rish.sh
+++ Init.d/rish.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
 export  PREFIX=/data/data/%package/files
 RISH_APPLICATION_ID=%package
-DEX=$PREFIX/lib/rish_shizuku.dex
+DEX=$PREFIX/rish_shizuku.dex
 /system/bin/app_process -Djava.class.path="$DEX" /system/bin --nice-name=rish rikka.shizuku.shell.ShizukuShellLoader "$@"
